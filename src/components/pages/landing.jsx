ec7141aade81688d3b2947c5dc4113560238728f
theaayushg <aayush.gupta@uconn.edu>
2023-11-11 14:10:03 -0500

    Finalized links

--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Dropdown } from "react-bootstrap";
 import Logo from "../images/logo.png";
 import Doctor from "../images/doc_1.jpg";
 import Virus from "../images/virusMolecule.png";
@@ -9,9 +9,18 @@ import { Link } from "react-router-dom";
 import "./styles/landing.css";
 
 const Immunify = () => {
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+  // Define an array of random locations (you can replace these with your actual locations)
+  const locations = ["Burundi", "Tanzania", "Mozambique", "Madagascar"];
+
+  // Function to handle location selection
+  const handleLocationSelect = (location) => {
+    setSelectedLocation(location);
+  };
+
   return (
     <div className="bodyDiv">
-      
       <img
         src={Virus}
         alt="Virus Molecule"
@@ -31,21 +40,32 @@ const Immunify = () => {
           fontFamily: "Droid Sans",
           fontWeight: "70",
         }}
-        class="d-flex justify-content-between"
+        className="d-flex justify-content-between"
       >
         <div className="d-flex">
-          <Link className="links">
+          <Link to="https://www.fda.gov/medical-devices/digital-health-center-excellence/fda-digital-health-advisory-committee" className="links">
             <li style={{ marginRight: "40px" }} className="navItem">
               Health Advisory
             </li>
           </Link>
-          <Link className="links">
-            <li className="navItem">
-              Location <img src={Drop} alt="Drop Down" />
-            </li>
-          </Link>
+          <Dropdown>
+            <Dropdown.Toggle variant="transparent" id="location-dropdown">
+              <span style={{ color: "white" }}>Location</span>{" "}
+              <img src={Drop} alt="Drop Down" />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {locations.map((location, index) => (
+                <Dropdown.Item
+                  key={index}
+                  onClick={() => handleLocationSelect(location)}
+                >
+                  {location}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
-        <div class="d-flex">
+        <div className="d-flex">
           <Link to="/login" className="links">
             <li style={{ marginRight: "40px" }} className="navItem">
               Login
@@ -63,26 +83,38 @@ const Immunify = () => {
             fontWeight: "700",
             fontFamily: "Droid Sans",
             fontSize: "45px",
+            fontWeight: "bold",
           }}
         >
           Mequity
         </p>
-        <Button
-          style={{
-            backgroundColor: "#A554F2",
-            border: "none",
-            fontSize: "12px",
-            width: "111px",
-            height: "41px",
-            borderRadius: "10px",
-          }}
-          className="navItem"
-        >
-          Contact us
-        </Button>
+        
+        <a href="mailto:mequity@gmail.com">
+          <Button
+            style={{
+              backgroundColor: "#A554F2",
+              border: "none",
+              fontSize: "12px",
+              width: "111px",
+              height: "41px",
+              borderRadius: "10px",
+            }}
+            className="navItem"
+          >
+            Contact us
+          </Button>
+        </a>
       </div>
 
-      <img src={Doctor} alt="Doctor with syringe" style={{position: "absolute", bottom:"0px"}} />
+      <p style={{ color: "white" }}>
+        Selected Location: {selectedLocation || "Select a location"}
+      </p>
+
+      <img
+        src={Doctor}
+        alt="Doctor with syringe"
+        style={{ position: "absolute", bottom: "0px" }}
+      />
     </div>
   );
 };
