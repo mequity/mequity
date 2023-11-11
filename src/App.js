e3160112de0426d52f4700508928c5ed4be78f21
aditya-chandraker <aditya.chandraker@uconn.edu>
2023-11-11 12:34:20 -0500

    landing page is fixed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import './App.css';
-import Landing from './components/pages/landing';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignUp from './components/pages/signUp';
-import SignUpTwo from './components/pages/experimental/signUpTwo';
-import LogIn from './components/pages/login';
-import Dashboard from './components/pages/dashboard';
-import Records from './components/pages/records';
-import Vaccines from './components/pages/vaccines';
-import Help from './components/pages/help';
-import Settings from './components/pages/settings';
+import "./App.css";
+import Landing from "./components/pages/landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./components/pages/signUp";
+import SignUpTwo from "./components/pages/experimental/signUpTwo";
+import LogIn from "./components/pages/login";
+import Dashboard from "./components/pages/dashboard";
+import Records from "./components/pages/records";
+import Vaccines from "./components/pages/vaccines";
+import Help from "./components/pages/help";
+import Settings from "./components/pages/settings";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div className="App">
-      <Routes>
-       <Route path="/" element={<Landing/>}/>
-       <Route path="/signup" element={<SignUp/>}/>
-       <Route path="/login" element={<LogIn/>}/>
-       <Route path="/signuptwo" element={<SignUpTwo/>}/>
-       <Route path="/dashboard" element={<Dashboard/>}/>
-       <Route path="/records" element={<Records/>}/>
-       <Route path="/vaccineTypes" element={<Vaccines/>}/>
-       <Route path="/help" element={<Help/>}/>
-       <Route path="/settings" element={<Settings/>}/>
-       
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signuptwo" element={<SignUpTwo />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/records" element={<Records />} />
+          <Route path="/vaccineTypes" element={<Vaccines />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -1,68 +1,90 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import Logo from '../images/logo.png';
-import Doctor from '../images/doctorWithSyringe.png';
-import Virus from '../images/virusMolecule.png';
-import Virus2 from '../images/virusMolecule2.png';
-import Virus3 from '../images/virusMolecule3.png';
-import Virus4 from '../images/virusMolecule4.png';
-import Virus5 from '../images/virusMolecule5.png';
-import Virus6 from '../images/virusMolecule6.png';
-import State from '../images/state.png';
-import City from '../images/city.png';
-import Search from '../images/search.png';
-import Drop from '../images/dropdown.png';
-import { Link } from 'react-router-dom';
-import './styles/landing.css'
-
+import React from "react";
+import { Button } from "react-bootstrap";
+import Logo from "../images/logo.png";
+import Doctor from "../images/doc_1.jpg";
+import Virus from "../images/virusMolecule.png";
+import Virus5 from "../images/virusMolecule5.png";
+import Drop from "../images/dropdown.png";
+import { Link } from "react-router-dom";
+import "./styles/landing.css";
 
 const Immunify = () => {
   return (
-    <div className='bodyDiv'>
+    <div className="bodyDiv">
+      
+      <img
+        src={Virus}
+        alt="Virus Molecule"
+        style={{ position: "absolute", left: "0px", top: "72px" }}
+      />
+      <img
+        src={Virus5}
+        alt="Virus Molecule"
+        style={{ position: "absolute", right: "0px", top: "400px" }}
+      />
 
-      <img src={Logo} alt="Logo" style={{position: 'absolute', left: "10px", top: "12px"}}/>    
-      <img src={Virus} alt="Virus Molecule" style={{position: 'absolute', left: "0px", top: "72px"}}/>
-      <img src={Virus2} alt="Virus Molecule" style={{position: 'absolute', left: "0px", top: "505px"}}/>
-      <img src={Virus3} alt="Virus Molecule" style={{position: 'absolute', left: "744px", top: "0px"}}/>
-      <Link to="signuptwo"><img src={Virus4} alt="Virus Molecule" style={{position: 'absolute', right: "0px", top: "339px"}}/></Link>
-      <img src={Virus5} alt="Virus Molecule" style={{position: 'absolute', right: "0px", bottom: "0px"}}/>
-      <img src={Virus6} alt="Virus Molecule" style={{position: 'absolute', left: "654px", bottom: "30px"}}/>
-
-      <nav style={{color:"white", listStyleType:"none", width:"75%", margin:"40px 0 0 300px", fontFamily:"Droid Sans", fontWeight:"700"}} class="d-flex justify-content-between">
-        <div  className="d-flex">
-          <Link className="links"><li style={{marginRight:"40px"}} className="navItem">Health Advisory</li></Link>
-          <Link className="links"><li className="navItem">Location <img src={Drop} alt="Drop Down"/></li></Link>
+      <nav
+        style={{
+          color: "white",
+          listStyleType: "none",
+          margin: "40px 0 0 50px",
+          fontFamily: "Droid Sans",
+          fontWeight: "70",
+        }}
+        class="d-flex justify-content-between"
+      >
+        <div className="d-flex">
+          <Link className="links">
+            <li style={{ marginRight: "40px" }} className="navItem">
+              Health Advisory
+            </li>
+          </Link>
+          <Link className="links">
+            <li className="navItem">
+              Location <img src={Drop} alt="Drop Down" />
+            </li>
+          </Link>
         </div>
-        <div  class="d-flex">
-          <Link to="/login" className="links"><li style={{marginRight:"40px"}} className="navItem">Login</li></Link>
-          <Link to="/signup" className="links"><li className="navItem">Sign Up</li></Link>
+        <div class="d-flex">
+          <Link to="/login" className="links">
+            <li style={{ marginRight: "40px" }} className="navItem">
+              Login
+            </li>
+          </Link>
+          <Link to="/signup" className="links">
+            <li className="navItem">Sign Up</li>
+          </Link>
         </div>
-
       </nav>
 
-        <div style={{color:"white",padding:"130px 0 0 300px"}}>
-            <h2 style={{fontWeight:"700", fontFamily:"Droid Sans", fontSize:"40px", lineHeight:"46px"}}>Find an <br/> Immunization <br/> centre near you</h2>
-            <p style={{width:"25%", margin:"20px 0 30px",fontWeight:"700", fontFamily:"Droid Sans", fontSize:"15px", lineHeight:"18.06px", color:"#C5A5F8"}}>Immunify seeks to help mothers locate the closest vaccination centre and also notify them of what vaccine their children need to take to reduce child mortality rate</p>
-            <Button style={{backgroundColor:"#A554F2", border:"none", fontSize:"12px", width: "111px", height: "41px", borderRadius: "10px"}} className="navItem">Contact us</Button>
-        </div>
-        
-        <div style={{position:"absolute", right:"200px", top:"150px"}}>
-          <img src={Doctor} alt="Doctor with syringe"/>
-        </div>
+      <div style={{ color: "white", padding: "150px 0 0 30px" }}>
+        <p
+          style={{
+            fontWeight: "700",
+            fontFamily: "Droid Sans",
+            fontSize: "45px",
+          }}
+        >
+          Mequity
+        </p>
+        <Button
+          style={{
+            backgroundColor: "#A554F2",
+            border: "none",
+            fontSize: "12px",
+            width: "111px",
+            height: "41px",
+            borderRadius: "10px",
+          }}
+          className="navItem"
+        >
+          Contact us
+        </Button>
+      </div>
 
-        <div style={{margin:"90px 0 0 300px", width:"600px", height: "180px", borderRadius:"15px", color:"white"}}>
-
-            <div className="actionCallTitle"><h2 style={{fontWeight:"700", textAlign:"center", paddingTop:"30px", fontFamily:"Droid Sans", fontSize:"30px"}}>Find an Immunization centre near you</h2></div>
-            <div className='d-flex justify-content-around actionCall' style={{height: "50%", paddingTop:"25px"}}>
-              <Button style={{backgroundColor:"white"}} className="navItem actionBtns"> <img src={State} alt="State" style={{marginRight:"8px"}}/>State</Button>
-              <Button style={{backgroundColor:"white"}} className="navItem actionBtns"> <img src={City} alt="City"style={{marginRight:"8px"}}/>City</Button>
-              <Button style={{backgroundColor:"#A554F2"}} className="navItem actionBtns"><img src={Search} alt="Search"/> Check</Button>
-            </div>
-
-        </div>
-
+      <img src={Doctor} alt="Doctor with syringe" style={{position: "absolute", bottom:"0px"}} />
     </div>
-  )
-}
+  );
+};
 
-export default Immunify
+export default Immunify;
